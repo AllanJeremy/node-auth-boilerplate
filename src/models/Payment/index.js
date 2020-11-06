ad42f6bac20a838202c0fadda1c6788f3242deff
AllanJeremy <aj.dev254@gmail.com>
2020-11-06 13:36:56 +0300

    [Refactoring] Models are now built on classes rather than through sequelize.define

--- src/models/Payment/index.js
+++ src/models/Payment/index.js
@@ -3,9 +3,9 @@
     This file does not implement any logic of its own and simply serves the purpose of importing models and exporting them out together so that they can be loaded from one file.
     Access to individual files is restricted only to files in the same directory or children of that directory
 */
-const Payment = require("./Payment");
+const Payment = require('./Payment');
 
 //* EXPORTS
 module.exports = {
-  Payment,
+    Payment
 };
