ae2563f004df49cada1d5f2ef03e27703486cf00
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 18:38:33 +0300

    [Feature] AdminLoggedIn functionality is now working as expected - This middleware must be called after the UserLoggedIn middleware as it relies on it to get the logged in user's information.
    - This separation of concerns (having different middleware for both) also allows us to process the user information and return extra data if necessary (in this case this could be extra admin related information)

--- src/middleware/PermissionMiddleware/AdminLoggedIn.js
+++ src/middleware/PermissionMiddleware/AdminLoggedIn.js
@@ -1,22 +1,34 @@
-//? Duplicate this  file and change the appropriate user
-const UserLoggedIn = require("./UserLoggedIn");
+// Utils
+const {
+  getApiUnauthorizedError,
+  printApiResponse,
+} = require("../../utils/api");
 
+//! For this file  to work, the user must be logged in (MUST be used after calling the UserLoggedIn middleware)
+//? Duplicate this  file and change `AUTH_DATA` to the appropriate user type
 // Config
 const { USER_TYPE } = require("../../config/auth");
-
-const AUTH_DATA = {
-  userTypeId: USER_TYPE.ADMIN,
-  userTypeTitle: "admin",
-};
+const printResponse = require("../../utils/api/printResponse");
 
 /** Only proceeds to the next middleware if a sales user is logged in based on the `AuthToken` passed in.
  *
  * Also sets `userData` on the `request` as `req.userData` that can be used by any middleware that comes after
  */
 const AdminLoggedIn = async (req, res, next) => {
-  req.authData = AUTH_DATA;
+  let userData = req.userData;
 
-  return UserLoggedIn(req, res, next);
+  // No point checking if we have the right user type logged in if we have no logged in user data to check
+  if (!userData) {
+    throw new Error(
+      "\nIt would appear the middleware was called before calling UserLoggedIn. Please call the UserLoggedIn middleware before calling this.\n"
+    );
+  } else if (userData.userTypeId === USER_TYPE.ADMIN) {
+    // Admin is indeed logged in
+    next();
+  } else {
+    let apiResponse = getApiUnauthorizedError();
+    printApiResponse(res, apiResponse, next);
+  }
 };
 
 //* EXPORTS
