5840c56bcd600b65e616ee2789a474363df46218
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 18:28:42 +0300

    [Enhancement] Added developer clarity to the attach error handler

--- src/utils/api/attachErrorHandler.js
+++ src/utils/api/attachErrorHandler.js
@@ -1,19 +1,30 @@
 // Internal
-const getError = require('./getError');
+const getError = require("./getError");
 
-/** Attaches an error handler to an `async` function 
+/** Attaches an error handler to an `async` function
  * @param {Object} res A HTTP resource object. Usually the first parameter of an Express middleware function
  * @param {Function} fn The `async` function to attach the error message to
  * @return A promise with an error handler attached to it
  */
-const attachErrorHandler = (res, fn) => { //? Intended to be called in middleware
-    return fn.catch(err => {
-        let apiResponse = getError(err.message, err);
-        console.log(err);
-        console.error(err.message);
+const attachErrorHandler = (res, fn) => {
+  //? Intended to be called in middleware
+  try {
+    return fn.catch((err) => {
+      let apiResponse = getError(err.message, err);
+      console.log(err);
+      console.error(err.message);
 
-        res.status(500).json(apiResponse);
+      res.status(500).json(apiResponse);
     });
+  } catch (err) {
+    if (err instanceof TypeError) {
+      console.error(
+        "The API error handler can only be attached to async functions.\nPlease make sure that the function you are passing as an argument to the error handler is an async function."
+      );
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 //* EXPORTS
