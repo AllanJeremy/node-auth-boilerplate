ba9d2469db1f6d0245f6109cbd71d7b0baeea446
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 17:31:45 +0300

    [Bugfix] Fixed issues with getting auth token - User data is now correctly returned by _getUserTokenData
    - getAuthToken is also getting the correct token from the database now though a relationship must be established between the auth token table and the user table at the model/migraiton level

---
+++ src/middleware/PermissionMiddleware/_getUserTokenData.js
@@ -0,0 +1,30 @@
+const { AuthService } = require("../../services");
+
+/** [Helper] Get token information and the user that owns the token
+ * @param {Object} req The request from a middleware. Should contain a bearer token.
+ */
+const _getUserTokenData = async (req) => {
+  // Set response data defaults ~ returned before we find user
+  let responseData = {
+    loggedIn: false,
+    tokenData: null,
+  };
+
+  // No need to go further if we don't have any authorization headers provided
+  if (!req.headers.authorization) return responseData;
+
+  // Get the token
+  const token = req.headers.authorization.split(" ")[1];
+  const tokenData = await AuthService.getAuthToken(token);
+
+  if (!tokenData || !tokenData.user) return responseData;
+
+  //* Getting here means that we found a token owner ~ user successfully logged in
+  responseData.loggedIn = true;
+  responseData.tokenData = tokenData;
+
+  return responseData;
+};
+
+//* EXPORTS
+module.exports = _getUserTokenData;
